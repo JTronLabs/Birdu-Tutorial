7acef8d81079a6c064599a6f59216b7bd8b79349
james <jtronlabs@gmail.com>
2017-03-28 21:44:23 -0400

    game restarts

--- src/states/GameOver.js
+++ src/states/GameOver.js
@@ -9,5 +9,10 @@ export default class GameOver extends Phaser.State {
     const txt = this.add.text(this.game.world.centerX, this.game.world.centerY, 'Game Over!',
       this.game.cache.getJSON('font_styles').score);
     txt.anchor.setTo(0.5, 0.5);
+
+    //start over the game
+    this.game.time.events.add(Phaser.Timer.SECOND, () => {
+      this.state.start('Game');
+    }, this);
   }
 }
